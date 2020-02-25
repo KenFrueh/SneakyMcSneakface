bf6075e0631180db90d13d1093bba5c12c147b14
Kenny  Frueh <kenfrueh@uat.edu>
2020-02-24 19:18:09 -0700

    Added HealthBar

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject TitleBackground;
+    public GameObject TitleText;
+    public GameObject GameStart;
+    public GameObject GameQuit;
     //Tracking score
     public int Score = 0;
+    public int Health = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +22,16 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }//Quit game
+    public void OnApplicationQuit()
+    {
+        Application.Quit();
+    }//Start game
+    public void StartGame()
+    {
+        TitleBackground.SetActive(false);
+        TitleText.SetActive(false);
+        GameStart.SetActive(false);
+        GameQuit.SetActive(false);
     }
 }
---
+++ Assets/Scripts/ProgessBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgessBar : MonoBehaviour
+{
+    public float CurrentHealth;//Current Health
+
+    public float MaxHealth = 100.0f;//Max Health
+
+    public Image HealthBar;//Health Bar reference
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;//Equaling values
+        HealthBar = gameObject.GetComponent<Image>();
+        HealthBar.type = Image.Type.Filled;
+        HealthBar.fillMethod = Image.FillMethod.Horizontal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float percentFilled = CurrentHealth / MaxHealth;
+        HealthBar.fillAmount = percentFilled;
+        if (percentFilled > .25)//Changing colors
+        {
+            HealthBar.color = Color.green;
+        }
+        else
+        {
+            HealthBar.color = Color.red;
+        }
+    }
+}
